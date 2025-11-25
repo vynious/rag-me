e4dab583846f2b4e429659367ce3c65fd4396581
vynious <shawnthiah@gmail.com>
2025-11-25 22:00:58 +0100

    encapsulate ai-related cmds in ai struct

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -3,13 +3,11 @@ pub mod inference;
 
 use anyhow::{Ok, Result};
 pub use embedding::EmbeddingEngine;
+use serde_json::json;
 
-use crate::ai::{
-    embedding::Embedder,
-    inference::{InferenceEngine, TextGeneration},
-};
+use crate::{ai::inference::InferenceEngine, data::database::VectorIndex};
 
-struct AI<E: EmbeddingEngine, I: InferenceEngine> {
+pub struct AI<E: EmbeddingEngine, I: InferenceEngine> {
     pub embedder: E,
     pub inferrer: I,
 }
@@ -17,5 +15,30 @@ struct AI<E: EmbeddingEngine, I: InferenceEngine> {
 impl<E: EmbeddingEngine, I: InferenceEngine> AI<E, I> {
     pub async fn new(embedder: E, inferrer: I) -> Result<Self> {
         Ok(AI { embedder, inferrer })
+    }
+
+    pub async fn answer_question_with_context(
+        &mut self,
+        query: &str,
+        references: &Vec<VectorIndex>,
+    ) -> Result<String> {
+        let mut context = Vec::new();
+        for reference in references.clone() {
+            context.push(json!({
+                "content": reference.content_chunk,
+                "metadata": reference.metadata
+            }));
+        }
+
+        let context_str = serde_json::to_string(&context)?;
+        let prompt = format!(
+            "You are a friendly AI agent. Context: {} Query: {}",
+            context_str, query
+        );
+        let response = self.inferrer.run(&prompt, 400).map_err(|e| {
+            eprintln!("error generating response: {}", e);
+            e
+        })?;
+        Ok(response)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use axum::Router;
-use lib::{cli, http};
+use lib::{
+    ai::{embedding::Embedder, inference::TextGeneration, AI},
+    cli, http,
+    utils::device,
+};
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let device = Arc::new(device(false)?);
+    let embedding_serivce = Embedder::new("").await?;
+    let inference_service = TextGeneration::new(
+        "Demonthos/dolphin-2_6-phi-2-candle",
+        398752958,
+        Some(0.8),
+        Some(0.7),
+        1.1,
+        64,
+        device.clone(),
+    )
+    .await?;
+    let ai_service = AI::new(embedder, inferrer);
+    
     let server_task = tokio::spawn(async {
         let app: Router = http::router();
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
